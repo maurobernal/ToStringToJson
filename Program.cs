690e9d970c94d09beb1879425bf3aabdd900ac96
Mauro Bernal <info@maurobernal.com.ar>
2023-04-27 20:21:35 -0300

    feat:MB: Example of ToString
    Plus. Add Json Serializer

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Text.Json;
+int numero = 2;
+string cadena = "Cadena";
+DateOnly fecha = new DateOnly(2023, 6, 30);
+
+System.Console.WriteLine(numero);
+System.Console.WriteLine(cadena);
+System.Console.WriteLine(fecha);
+
+Alumnos alumno = new() { Id = 2, Nombres = "Mauro", Apellidos = "Bernal" };
+
+System.Console.WriteLine(alumno);
+System.Console.WriteLine(alumno.ToString());
+
+System.Console.WriteLine(JsonSerializer.Serialize(alumno));
+
+
+public class Alumnos
+{
+    public int Id { get; set; }
+    public string Nombres { get; set; } = string.Empty;
+    public string Apellidos { get; set; } = string.Empty;
+
+    /*public override string ToString()
+    => $"Nombres:{Nombres}, Apellidos:{Apellidos}";*/
+
+    public override string ToString()
+ => JsonSerializer.Serialize(this, new JsonSerializerOptions() { WriteIndented = true });
+}
